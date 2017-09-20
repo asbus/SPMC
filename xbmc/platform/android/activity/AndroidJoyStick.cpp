c98a25a88d49961b5b7a1e3c7188040b44542c29
Chris Koying Browet <cbro@semperpax.com>
2017-09-20 18:12:11 +0200

    debug

--- xbmc/platform/android/activity/AndroidJoyStick.cpp
+++ xbmc/platform/android/activity/AndroidJoyStick.cpp
@@ -27,11 +27,12 @@ bool CAndroidJoyStick::onJoyStickEvent(AInputEvent* event)
 {
   int32_t source = AInputEvent_getSource(event);
 
+  CXBMCApp::android_printf("CAndroidJoyStick::onJoyStickEvent(type = %d, keycode = %d, source = %d)",
+                           AInputEvent_getType(event), AKeyEvent_getKeyCode(event), source);
+
   // only handle input events from a gamepad or joystick
   if ((source & (AINPUT_SOURCE_GAMEPAD | AINPUT_SOURCE_JOYSTICK)) != 0)
     return CXBMCApp::onInputDeviceEvent(event);
 
-  CXBMCApp::android_printf("CAndroidJoyStick::onJoyStickEvent(type = %d, keycode = %d, source = %d): ignoring non-joystick input event",
-                           AInputEvent_getType(event), AKeyEvent_getKeyCode(event), source);
   return false;
 }
